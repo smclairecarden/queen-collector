2bfe39caaa53b73b79d7a877e59a7d2cfa2862f9
Claire Carden <smclairecarden@gmail.com>
2022-03-04 20:22:50 -0600

    add create and show functionality

--- controllers/queens.js
+++ controllers/queens.js
@@ -14,6 +14,38 @@ function index(req, res) {
   })
 }
 
+function newQueen(req, res) {
+  res.render('queens/new', {
+    title: 'Add A Queen'
+  })
+}
+
+function create(req, res) {
+  for(let key in req.body) {
+    if(req.body[key] === '') delete req.body[key]
+  }
+  const queen = new Queen(req.body)
+  queen.save(function(err) {
+    if (err) {
+      return res.redirect('queens/new', 
+      {err:null, title:'Add Queen'})
+    }
+    res.redirect(`/queens/${queen._id}`)
+  })
+}
+
+function show(req, res) {
+  Queen.findById(req.params.id, function (err, queen) {
+    res.render('queens/show', {
+      title: 'All About This Queen',
+      queen: queen,
+    })
+  })
+}
+
 export {
-  index
+  index,
+  newQueen as new,
+  create,
+  show,
 }
--- routes/queens.js
+++ routes/queens.js
@@ -4,6 +4,10 @@ import * as queensCtrl from '../controllers/queens.js'
 const router = Router()
 
 router.get('/', queensCtrl.index)
+router.get('/new', queensCtrl.new)
+router.get('/:id', queensCtrl.show)
+
+router.post('/', queensCtrl.create)
 
 export {
   router
