685357d8301e682ec9718e347fde167aa8dbe0ff
Claire Carden <smclairecarden@gmail.com>
2022-03-10 14:57:50 -0600

    style edit and add pages

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,60 +3,60 @@ import { Queen } from '../models/queen.js'
 
 function index(req, res) {
   Profile.find({})
-  .then(profiles => {
-    res.render('profiles/index', {
-      profiles,
-      title: 'Users'
+    .then(profiles => {
+      res.render('profiles/index', {
+        profiles,
+        title: 'Users'
+      })
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect(`/profiles/${req.user.profile._id}`)
-  }) 
+    .catch(err => {
+      console.log(err)
+      res.redirect(`/profiles/${req.user.profile._id}`)
+    })
 }
 
 function show(req, res) {
   Profile.findById(req.params.id)
-  .populate('favorites')
-  .then(self => {
-    const isSelf = self._id.equals(req.params.profileId)
-    console.log(req.params.profileId)
-    res.render('profiles/show', {
-      title: `${self.name}'s Profile`,
-      profile: self,
-      isSelf,
+    .populate('favorites')
+    .then(self => {
+      const isSelf = self._id.equals(req.params.profileId)
+      console.log(req.params.profileId)
+      res.render('profiles/show', {
+        title: `${self.name}'s Profile`,
+        profile: self,
+        isSelf,
+      })
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/')
-  })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
 }
 
 function showQueen(req, res) {
   Queen.findById(req.params.queenId)
-  .populate(req.params.profileId)
+    .populate(req.params.profileId)
     .then(queen => {
       res.render('profiles/show', {
         title: `${profile.name}'s Profile`,
         queen,
       })
     })
-  }
+}
 
 function deleteFavorite(req, res) {
   Profile.findById(req.user.profile._id)
-  .then(profile => {
-    profile.favorites.remove({_id:  req.params.id})
-    profile.save()
-    .then(() => {
+    .then(profile => {
+      profile.favorites.remove({ _id: req.params.id })
+      profile.save()
+        .then(() => {
+          res.redirect(`/profiles/${req.user.profile._id}`)
+        })
+    })
+    .catch(err => {
+      console.log(err)
       res.redirect(`/profiles/${req.user.profile._id}`)
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect(`/profiles/${req.user.profile._id}`)
-  })
 }
 
 export {
--- controllers/queens.js
+++ controllers/queens.js
@@ -3,20 +3,20 @@ import { Queen } from '../models/queen.js'
 
 function index(req, res) {
   Queen.find({})
-  .then(queens =>  {
-    queens = queens.sort((a, b) => {
-      return parseInt(a.season) - parseInt(b.season)
+    .then(queens => {
+      queens = queens.sort((a, b) => {
+        return parseInt(a.season) - parseInt(b.season)
+      })
+      console.log(queens)
+      res.render('queens/index', {
+        queens,
+        title: 'The Queens'
+      })
     })
-    console.log(queens)
-    res.render('queens/index', {
-      queens,
-      title: 'The Queens'
+    .catch(err => {
+      console.log(err)
+      res.redirect("/tacos")
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect("/tacos")
-  })
 }
 
 function newQueen(req, res) {
@@ -28,98 +28,98 @@ function newQueen(req, res) {
 function create(req, res) {
   req.body.owner = req.user.profile._id
   Queen.create(req.body)
-  .then(queen => {
-    res.redirect('/queens')
-  }) 
-  .catch(err => {
-    console.log(err)
-    res.redirect('/queens')
-  })
+    .then(queen => {
+      res.redirect('/queens')
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/queens')
+    })
 }
 
 function show(req, res) {
   Queen.findById(req.params.id)
-  .populate("owner")
-  .populate("reads.author")
-  .then(queen => {
-    res.render('queens/show', {
-      title: 'All About This Queen',
-      queen,
+    .populate("owner")
+    .populate("reads.author")
+    .then(queen => {
+      res.render('queens/show', {
+        title: 'All About This Queen',
+        queen,
+      })
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/queens')
-  })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/queens')
+    })
 }
 
 function edit(req, res) {
   Queen.findById(req.params.id)
-  .then(queen => {
-    res.render('queens/edit', {
-      title: 'Edit Queen',
-      queen
+    .then(queen => {
+      res.render('queens/edit', {
+        title: 'Edit Queen',
+        queen
+      })
     })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/queens')
-  })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/queens')
+    })
 }
 
 function update(req, res) {
   Queen.findById(req.params.id)
-  .then(queen => {
-    if(queen.owner.equals(req.user.profile._id)) {
-      queen.updateOne(req.body, {new: true})
-      .then(() => {
-        res.redirect(`/queens/${queen._id}`)
-      })
-    } else {
-      throw new Error ('NOT AUTHORIZED')
-    }
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/queens')
-  })
+    .then(queen => {
+      if (queen.owner.equals(req.user.profile._id)) {
+        queen.updateOne(req.body, { new: true })
+          .then(() => {
+            res.redirect(`/queens/${queen._id}`)
+          })
+      } else {
+        throw new Error('NOT AUTHORIZED')
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/queens')
+    })
 }
 
 function createRead(req, res) {
-  Queen.findById(req.params.id, function(err, queen) {
+  Queen.findById(req.params.id, function (err, queen) {
     req.body.author = req.user.profile._id
     queen.reads.push(req.body)
-    queen.save(function(err) {
+    queen.save(function (err) {
       res.redirect(`/queens/${queen._id}`)
     })
   })
 }
 
 function deleteQueen(req, res) {
- Queen.findByIdAndDelete(req.params.id, function(err, queen) {
-   res.redirect("/queens")
- })
+  Queen.findByIdAndDelete(req.params.id, function (err, queen) {
+    res.redirect("/queens")
+  })
 }
 
 function addFavorite(req, res) {
   console.log('test')
   Profile.findById(req.params.profileId)
-  .then(profile => {
-    Queen.findById(req.params.queenId)
-    .then(queen => {
-      if(profile.favorites.includes(req.params.queenId)) {
-        console.log('You have already favorited this queen!')
-        res.redirect(`/queens/${req.params.queenId}`)
-      } else {
-        profile.favorites.push(req.params.queenId)
-        profile.save()
-        res.redirect(`/queens/${req.params.queenId}`)
-      }
-    }).catch(err => {
-    console.log(err)
-    res.redirect(`/queens`)
+    .then(profile => {
+      Queen.findById(req.params.queenId)
+        .then(queen => {
+          if (profile.favorites.includes(req.params.queenId)) {
+            console.log('You have already favorited this queen!')
+            res.redirect(`/queens/${req.params.queenId}`)
+          } else {
+            profile.favorites.push(req.params.queenId)
+            profile.save()
+            res.redirect(`/queens/${req.params.queenId}`)
+          }
+        }).catch(err => {
+          console.log(err)
+          res.redirect(`/queens`)
+        })
     })
-  })
 }
 
 export {
